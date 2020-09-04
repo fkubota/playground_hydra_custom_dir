aa045335457d3dfd85547a292a4b8533591284c7
fkubota <rkkubotay@gmail.com>
2020-09-04 15:57:37 +0900

    [Update]

--- run.py
+++ run.py
@@ -13,6 +13,8 @@ def run(cfg: DictConfig) -> None:
     logger.info('logger start')
     logger.info(f'all params\n{"="*70}\n{cfg.pretty()}\n{"="*70}')
     hello.hello()
+    import os
+    logger.info(os.getcwd().split('/hydra_outputs/'))
 
 
 if __name__ == "__main__":
